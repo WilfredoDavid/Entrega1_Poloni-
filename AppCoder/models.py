a8cc684e06fe2c6750cea3b0ea4654ed44a87dfe
WilfredoDavid <wilfredodro311@gmail.com>
2022-08-31 19:45:39 -0300

    Models agregados

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -3,9 +3,20 @@ from django.db import models
 # Create your models here.
 # Se crea la clase familia
 
-class Auto(models.Model):
+class Paises(models.Model):
+    nombre=models.CharField(max_length=20)
+    continente=models.CharField(max_length=20)
+    empresas_automotrices=models.CharField(max_length=50)
+      
+class Empresas(models.Model):
+    nombre=models.CharField(max_length=30)
+    fecha_creacion=models.DateField()
+    area=models.CharField(max_length=30)
 
-    marca=models.CharField(max_length=40)
-    modelo=models.CharField(max_length=40)
-    anio_lanzamiento=models.IntegerField()
+class Autos(models.Model):
+    modelo=models.CharField(max_length=20)
+    fecha_lanzamiento=models.DateField()
+    velocidad_maxima=models.IntegerField()
+    transmision_automatica=models.CharField(max_length=10)
+    transmision_manual=models.CharField(max_length=10)
   
